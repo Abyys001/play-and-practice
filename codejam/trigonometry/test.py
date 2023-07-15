f47a55939953710fcae6a9767c20d6085e3ad2f0
Siavash <119043855+Abyys001@users.noreply.github.com>
2023-07-15 23:30:10 +0330

    practice

--- codejam/trigonometry/test.py
+++ codejam/trigonometry/test.py
@@ -1,5 +1,16 @@
-nums = [1, 2, 3, 5]
+rec1 = [2, 4, 4]
+rec4 = [5, 2, 6]
+rec3 = [4, 2, 5]
+rec2 = [5, 4, 6]
+z
+center_rec = rec1
 
-for i in range(len(nums)):
-    print(i)
-    
+for i in range(len(center_rec)):
+    is_equal = 0
+    for num in center_rec:
+        if center_rec[i] == num:
+            is_equal += 1
+    if is_equal >= 2:
+        print(True)
+    else:
+        print(False)
--- codejam/trigonometry/trigonometry.py
+++ codejam/trigonometry/trigonometry.py
@@ -1,10 +1,12 @@
 # usr/bin/python3
 
 """
-You can make a big triangle with four triangles
+You can make a big triangle with four triangles?!
+In the four input lines, all three sides of the triangle are placed as input, and the output is yes or no.
 """
 
 sides = input()
+
 
 def main(inputs):
     pass
